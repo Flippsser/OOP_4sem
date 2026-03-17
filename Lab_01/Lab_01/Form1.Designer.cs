7b0c5d5d76148dd5025e6bed6364493ba231a370
Mikhaill <mishabraz07@gmail.com>
2026-03-17 19:06:15 +0300

    зачтено

---
+++ Lab_01/Lab_01/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Lab_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(58, 49);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "метры";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(58, 101);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(64, 20);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "футы";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(58, 153);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(112, 20);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "килограммы ";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(58, 206);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(75, 20);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "фунты ";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(58, 261);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(69, 20);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "литры";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(58, 314);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(84, 20);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "галлоны";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(667, 314);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(84, 20);
+            this.checkBox7.TabIndex = 11;
+            this.checkBox7.Text = "галлоны";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(667, 261);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(69, 20);
+            this.checkBox8.TabIndex = 10;
+            this.checkBox8.Text = "литры";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.checkBox8_CheckedChanged);
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(667, 206);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(75, 20);
+            this.checkBox9.TabIndex = 9;
+            this.checkBox9.Text = "фунты ";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            this.checkBox9.CheckedChanged += new System.EventHandler(this.checkBox9_CheckedChanged);
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(667, 153);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(112, 20);
+            this.checkBox10.TabIndex = 8;
+            this.checkBox10.Text = "килограммы ";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            this.checkBox10.CheckedChanged += new System.EventHandler(this.checkBox10_CheckedChanged);
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(667, 101);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(64, 20);
+            this.checkBox11.TabIndex = 7;
+            this.checkBox11.Text = "футы";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            this.checkBox11.CheckedChanged += new System.EventHandler(this.checkBox11_CheckedChanged);
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.AutoSize = true;
+            this.checkBox12.Location = new System.Drawing.Point(667, 49);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Size = new System.Drawing.Size(70, 20);
+            this.checkBox12.TabIndex = 6;
+            this.checkBox12.Text = "метры";
+            this.checkBox12.UseVisualStyleBackColor = true;
+            this.checkBox12.CheckedChanged += new System.EventHandler(this.checkBox12_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "In";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(671, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Out";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(367, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Перевод";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(356, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "Вход";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(356, 274);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.Text = "Выход";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.checkBox11);
+            this.Controls.Add(this.checkBox12);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.CheckBox checkBox12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Lab_01/Lab_01/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Lab_01
+{
+    public partial class Form1 : Form
+    {
+        private readonly Calculator _calculator;
+        private readonly CheckBox[] _inBoxes;
+        private readonly CheckBox[] _outBoxes;
+        private readonly string[] _units = { "метры", "футы", "килограммы", "фунты", "литры", "галлоны" };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _calculator = new Calculator();
+            _calculator.ConversionCompleted += (result, from, to) => textBox2.Text = $"{result:F4}";
+            _calculator.ConversionError += msg => MessageBox.Show(msg, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            _inBoxes = new[] { checkBox1, checkBox2, checkBox3, checkBox4, checkBox5, checkBox6 };
+            _outBoxes = new[] { checkBox12, checkBox11, checkBox10, checkBox9, checkBox8, checkBox7 };
+            checkBox1.Checked = true;
+            checkBox11.Checked = true;
+        }
+
+        // Общий обработчик для всех чекбоксов
+        private void CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sender is CheckBox current && current.Checked)
+            {
+                // Определяем группу (In или Out)
+                CheckBox[] group = Array.IndexOf(_inBoxes, current) >= 0 ? _inBoxes : _outBoxes;
+                foreach (var cb in group)
+                    if (cb != current) cb.Checked = false;
+            }
+        }
+
+        // Индивидуальные обработчики для дизайнера
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+        private void checkBox3_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+        private void checkBox4_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+        private void checkBox5_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+        private void checkBox6_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+        private void checkBox7_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+        private void checkBox8_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+        private void checkBox9_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+        private void checkBox10_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+        private void checkBox11_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+        private void checkBox12_CheckedChanged(object sender, EventArgs e) => CheckBox_CheckedChanged(sender, e);
+
+        // Обработчик кнопки
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string from = GetSelected(_inBoxes);
+                string to = GetSelected(_outBoxes);
+
+                if (from == null) throw new InvalidOperationException("Выберите единицу в группе In.");
+                if (to == null) throw new InvalidOperationException("Выберите единицу в группе Out.");
+
+                if (!double.TryParse(textBox1.Text, out double value) || value < 0)
+                    throw new FormatException("Введите корректное число.");
+
+                _calculator.Convert(from, to, value);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // Получение выбранной единицы по индексу в массиве
+        private string GetSelected(CheckBox[] boxes)
+        {
+            for (int i = 0; i < boxes.Length; i++)
+                if (boxes[i].Checked) return _units[i];
+            return null;
+        }
+
+        // Пустые обработчики для дизайнера
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+    }
+
+    // Делегат и класс калькулятора
+    public delegate double ConversionDelegate(double value);
+
+    public class Calculator
+    {
+        public event Action<double, string, string> ConversionCompleted;
+        public event Action<string> ConversionError;
+
+        private readonly Dictionary<(string from, string to), ConversionDelegate> _converters;
+
+        public Calculator()
+        {
+            _converters = new Dictionary<(string, string), ConversionDelegate>
+            {
+                { ("метры", "футы"), m => m * 3.28084 },
+                { ("футы", "метры"), f => f / 3.28084 },
+                { ("килограммы", "фунты"), kg => kg * 2.20462 },
+                { ("фунты", "килограммы"), lb => lb / 2.20462 },
+                { ("литры", "галлоны"), l => l * 0.264172 },
+                { ("галлоны", "литры"), gal => gal / 0.264172 }
+            };
+        }
+
+        public void Convert(string fromUnit, string toUnit, double value)
+        {
+            try
+            {
+                if (_converters.TryGetValue((fromUnit, toUnit), out ConversionDelegate converter))
+                {
+                    double result = converter(value);
+                    ConversionCompleted?.Invoke(result, fromUnit, toUnit);
+                }
+                else
+                {
+                    ConversionError?.Invoke($"Конвертация из '{fromUnit}' в '{toUnit}' не поддерживается.");
+                }
+            }
+            catch (Exception ex)
+            {
+                ConversionError?.Invoke($"Ошибка при конвертации: {ex.Message}");
+            }
+        }
+    }
+}
